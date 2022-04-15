3b1523063921fd47f7acc8583d69af3f4f7f5d5d
capslockhuh <99589445+Capslockhuh@users.noreply.github.com>
2022-04-15 08:53:36 +0200

    Add files via upload

---
+++ PhotoDisplay/PhotoDisplay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoDisplay
+//
+//  Created by Jan Andrzejewski on 13/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
